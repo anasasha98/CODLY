c661edb5aa7b96daf806f7ab19a2e1d324b054fc
anas asha <96749174+anasasha98@users.noreply.github.com>
2022-04-25 21:39:27 -0700

    now

---
+++ forms/connection.php
@@ -0,0 +1,26 @@
+<?php
+
+$username = "root";
+$password = "";
+$server = "localhost";
+$db = "codly";
+
+
+$con = mysqli_connect($server, $username, $password , $db);
+
+
+if($con){
+    ?>
+       <script>
+           alert('Connection Successful')
+       </script>
+    <?php
+   
+}else{
+    
+    die("Connection Failed" . mysqli_connect_error()); 
+}
+
+
+
+?>
--- sign-up.php
+++ sign-up.php
@@ -2,10 +2,10 @@
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8" />
+  <meta charset="UTF-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>Sign in</title>
+  <title>Sign Up</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
@@ -14,9 +14,7 @@
   <!-- <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon" /> -->
 
   <!-- Google Fonts -->
-  <link
-    href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Jost:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
-    rel="stylesheet" />
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Jost:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet" />
 
   <!-- Vendor CSS Files -->
   <link href="assets/vendor/aos/aos.css" rel="stylesheet" />
@@ -28,17 +26,12 @@
 
   <!-- Template Main CSS File -->
   <link href="assets/css/style.css" rel="stylesheet" />
-
-
-
-
 </head>
 
 <body>
 
   <main>
     <div class="container">
-
       <section class="section register min-vh-100 d-flex flex-column align-items-center justify-content-center py-4">
         <div class="container">
           <div class="row justify-content-center">
@@ -49,54 +42,63 @@
 
                   <span class="d-none d-lg-block">Codly</span>
                 </a>
-              </div>
-
-
+              </div><!-- End Logo -->
 
               <div class="card mb-3">
 
                 <div class="card-body">
 
                   <div class="pt-4 pb-2">
-                    <h5 class="card-title text-center pb-0 fs-4">Sign in to Your Account</h5>
-                    <p class="text-center small">Enter your username & password to Sign in</p>
+                    <h5 class="card-title text-center pb-0 fs-4">Create an Account</h5>
+                    <p class="text-center small">Enter your personal details to create account</p>
                   </div>
 
-                  <form class="row g-3 needs-validation">
+                  <form method="POST" action="" class="row g-3 needs-validation">
+                    <div class="col-12">
+                      <label for="yourName" class="form-label">* Your Name</label>
+                      <input type="text" name="name" class="form-control" id="yourName" required maxlength="50">
+                      <div class="invalid-feedback">Please, enter your name!</div>
+                    </div>
 
                     <div class="col-12">
-                      <label for="yourUsername" class="form-label">Username</label>
+                      <label for="yourEmail" class="form-label">* Your Email</label>
+                      <input type="email" name="email" class="form-control" id="yourEmail" required maxlength="50">
+                      <div class="invalid-feedback">Please enter a valid Email adddress!</div>
+                    </div>
+
+                    <div class="col-12">
+                      <label for="yourUsername" class="form-label">* Username</label>
                       <div class="input-group has-validation">
                         <span class="input-group-text" id="inputGroupPrepend">@</span>
-                        <input type="text" name="username" class="form-control" id="yourUsername" required>
-                        <div class="invalid-feedback">Please enter your username.</div>
+                        <input type="text" name="username" class="form-control" id="yourUsername" required maxlength="50">
+                        <div class="invalid-feedback">Please choose a username.</div>
                       </div>
                     </div>
 
                     <div class="col-12">
-                      <label for="yourPassword" class="form-label">Password</label>
-                      <input type="password" name="password" class="form-control" id="yourPassword" required>
+                      <label for="yourPassword" class="form-label">* Password</label>
+                      <input type="password" name="password" class="form-control" id="yourPassword" required maxlength="50">
                       <div class="invalid-feedback">Please enter your password!</div>
                     </div>
 
                     <div class="col-12">
                       <div class="form-check">
-                        <input class="form-check-input" type="checkbox" name="remember" value="true" id="rememberMe">
-                        <label class="form-check-label" for="rememberMe">Remember me</label>
+                        <input class="form-check-input" name="terms" type="checkbox" value="" id="acceptTerms" required>
+                        <label class="form-check-label" for="acceptTerms">I agree and accept the
+                          <a href="#">terms and conditions</a></label>
+                        <div class="invalid-feedback">You must agree before submitting.</div>
                       </div>
                     </div>
                     <div class="col-12">
-                      <button class="btn btn-primary w-100" type="submit">Sign in</button>
+                      <button class="btn btn-primary w-100" type="submit">Create Account</button>
                     </div>
                     <div class="col-12">
-                      <p class="small mb-0">Don't have account? <a href="sign-up.html">Create an account</a></p>
+                      <p class="small mb-0">Already have an account? <a href="sign-in.php">Log in</a></p>
                     </div>
                   </form>
 
-
                 </div>
               </div>
-
 
 
             </div>
@@ -108,8 +110,7 @@
     </div>
   </main><!-- End #main -->
 
-  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
-      class="bi bi-arrow-up-short"></i></a>
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
   <!-- Vendor JS Files -->
   <script src="assets/vendor/aos/aos.js"></script>
