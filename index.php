ef12460aad3dd35b62a48ddad89603b4bd4b2321
Ali-Ziadeh <ali.h.ziadeh@outlook.com>
2022-05-01 00:43:26 +0300

    payment form

--- index.php
+++ index.php
@@ -379,7 +379,8 @@ if ($con) {
         Designed by <a href="#">IT Development Team</a>
       </div>
     </div>
-  </footer><!-- End Footer -->
+  </footer>
+  <!-- End Footer -->
   <div id="preloader"></div>
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
