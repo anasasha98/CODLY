609446780732048128897984160e99686a6ca4f2
Ali-Ziadeh <ali.h.ziadeh@outlook.com>
2022-06-02 20:26:56 +0300

    again

--- admin/include/session.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-include '../../forms/connection.php';
-if(empty($_SESSION['username'] && $_SESSION['password'])){
-    header("location: ../signin.php");
-
-}
--- logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-unset($_SESSION["username"]);
-unset($_SESSION["type"]);
-
-// echo 'You have cleaned session';
-header('Refresh: 1; URL = index.php');
